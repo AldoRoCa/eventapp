7e83964ea9ddf92a459dc86cdc1b28fb0adcb094
AldoRoCa <aldodrc2007@gmail.com>
2026-06-10 00:45:15 -0600

    feat: Sentry error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from "@sentry/react"
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: "https://05e09a00446bcc42915def690405fe7d@o4511539764854784.ingest.us.sentry.io/4511539772784640",
+  environment: import.meta.env.MODE,
+  tracesSampleRate: 0.2,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
